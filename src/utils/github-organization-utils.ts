afdff57c47d9aee23e8687c39b52c5607e045826
Essomatic <teemu.pussinen9@gmail.comm>
2024-08-08 10:40:40 +0300

    added page pagination & filter out forks

--- src/utils/github-organization-utils.ts
+++ src/utils/github-organization-utils.ts
@@ -4,7 +4,7 @@ import { Repository } from "../types/types";
 const octokit = new Octokit({ });
 
 /**
- * Fetches all public repositories belonging to the specified organization from GitHub and filters out archived repos.
+ * Fetches all public repositories belonging to the specified organization from GitHub and filters out archived/forked repos.
  * 
  * @param org - The name of the organization.
  * @returns A Promise that resolves to an array of Repository objects.
@@ -12,25 +12,41 @@ const octokit = new Octokit({ });
  */
 export async function getOrganizationPublicRepositories(org: string): Promise<Repository[]> {
   try {
-    const response = await octokit.request("GET /orgs/{org}/repos", {
-      org,
-      type: "public",
-      per_page: 100, 
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28"
+    const repositories: Repository[] = [];
+    let page = 1;
+    let hasMore = true;
+
+    while (hasMore) {
+      const response = await octokit.request("GET /orgs/{org}/repos", {
+        org,
+        type: "public",
+        per_page: 100,
+        page,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28"
+        }
+      });
+
+      const nonForkedNonArchivedRepos = response.data.filter((repo: any) => !repo.fork && !repo.archived);
+
+      const pageRepositories = nonForkedNonArchivedRepos.map((repo: any) => ({
+        owner: repo.owner.login,
+        repository: repo.name
+      }));
+
+      repositories.push(...pageRepositories);
+
+      if (response.data.length < 100) {
+        hasMore = false;
+      } else {
+        page++;
       }
-    });
+    }
 
-    const nonArchivedRepos = response.data.filter(repo => !repo.archived);
-
-    const repositories = nonArchivedRepos.map((repo: any) => ({
-      owner: repo.owner.login,
-      repository: repo.name
-    }));
     return repositories;
 
   } catch (error) {
     console.error("Error fetching repositories:", error);
-    throw error
+    throw error;
   }
 };
